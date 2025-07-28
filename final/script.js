3dd2b9b4d632f3b083da6b2085b51345fb901dbf
Yeseul Kim <dptmfl3018@naver.com>
2025-07-28 19:06:52 +0900

    html 변경

--- final/script.js
+++ final/script.js
@@ -1,45 +1,43 @@
 // home 텍스트 타이핑 효과
-const content = "H E L L O !"
-const text = document.querySelector(".title h1");
+const content = "H E L L O !";
+const text = document.querySelector(".title .typing");
 let textIdx = 0;
 
 function typing() {
-    let txt = content[textIdx++];
+  let txt = content[textIdx++];
 
-    text.innerHTML += txt;
+  text.innerHTML += txt;
 
-    if (textIdx > content.length) {
-        text.textContent = "";
-        textIdx = 0;
-    }
+  if (textIdx > content.length) {
+    text.textContent = "";
+    textIdx = 0;
+  }
 }
 
-setInterval(typing, 200)
-
+setInterval(typing, 200);
 
 // 스크롤에 반응하는 애니메이션
-let aboutCard = document.querySelector(".about_profile")
+let aboutCard = document.querySelector(".about_profile");
 
-let contactTitle = document.querySelector("#contact .category h2")
+let contactTitle = document.querySelector("#contact .category h2");
 let contactLink = document.querySelector("#contact .link ul");
 
-window.addEventListener("scroll", function() {
-    let value = window.scrollY;
-    if (value < 400){
-        aboutCard.style.animation = "about_backslide 0.5s ease-out forwards";
-    } else {
-        aboutCard.style.animation = "about_slide 0.5s ease-out"
-    };
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  if (value < 400) {
+    aboutCard.style.animation = "about_backslide 0.5s ease-out forwards";
+  } else {
+    aboutCard.style.animation = "about_slide 0.5s ease-out";
+  }
 
-    if(value > 2500) {
-        contactTilte.style.animation = "grow 0.8s linear";
-        contactLink.style.animation = "grow 0.8s linear";
-    } else{
-        contactTilte.style.animation = "grow-out 0.5s linear";
-        contactLink.style.animation = "grow-out 0.5s linear";
-    };
-})
-
+  if (value > 2500) {
+    contactTilte.style.animation = "grow 0.8s linear";
+    contactLink.style.animation = "grow 0.8s linear";
+  } else {
+    contactTilte.style.animation = "grow-out 0.5s linear";
+    contactLink.style.animation = "grow-out 0.5s linear";
+  }
+});
 
 // project slide
 const slides = document.querySelector(".slides");
@@ -53,29 +51,30 @@ let slideMargin = 30;
 
 const visibleCount = 2;
 
-slides.style.width = (slideWidth + slideMargin) * slideCount - slideMargin + 'px';
+slides.style.width =
+  (slideWidth + slideMargin) * slideCount - slideMargin + "px";
 
 const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next")
+const nextBtn = document.querySelector(".next");
 
-function moveSlide(num){
-    // slide 가 움직일때 slide index가 1이면 -330px 2면 -660px
-    slides.style.left = -num * 430 +'px';
-    currentIdx = num;
+function moveSlide(num) {
+  // slide 가 움직일때 slide index가 1이면 -330px 2면 -660px
+  slides.style.left = -num * 430 + "px";
+  currentIdx = num;
 }
 
-prevBtn.addEventListener('click', function(){
-    if(currentIdx > 0){
-        moveSlide(currentIdx - 1);
-    } else {
-        moveSlide(slideCount - visibleCount);
-    }
-})
+prevBtn.addEventListener("click", function () {
+  if (currentIdx > 0) {
+    moveSlide(currentIdx - 1);
+  } else {
+    moveSlide(slideCount - visibleCount);
+  }
+});
 
-nextBtn.addEventListener('click', function(){
-    if(currentIdx < slideCount - visibleCount){
-        moveSlide(currentIdx + 1);
-    } else {
-        moveSlide(0);
-    }
-})
+nextBtn.addEventListener("click", function () {
+  if (currentIdx < slideCount - visibleCount) {
+    moveSlide(currentIdx + 1);
+  } else {
+    moveSlide(0);
+  }
+});
